1ccd1d06c484f0ad3d4ab83eebc3afcc69053748
querythetry <adrian.leon.alonso@gmail.com>
2024-04-22 00:36:30 +0200

    delete partytown

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-import partytown from "@astrojs/partytown";
-
-// https://astro.build/config
-export default defineConfig({
-  site: 'https://al375840.github.io',
-  integrations: [partytown()]
-});
